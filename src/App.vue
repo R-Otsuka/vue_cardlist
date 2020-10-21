46baba51d51977fe08b8baeacd413c14b5849313
kamio06 <yakugozy@gmail.com>
2020-10-21 17:46:49 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- ★Boardコンポーネント呼び出し -->
+    <board />
+  </div>
+</template>
+<script>
+  import Board from './components/Board.vue'
+  export default {
+    name: 'app',
+    components: {
+      // ★コンポーネントに登録する
+      Board
+    }
+  }
+</script>
