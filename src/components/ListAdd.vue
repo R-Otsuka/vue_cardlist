2e9b43aacfc4ad35f5fbe7e72bf84749b4f69f00
kamio06 <yakugozy@gmail.com>
2020-10-21 20:48:02 +0900

    add-Listcomponent

---
+++ src/components/ListAdd.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- ★ここから追記 -->
+  <!-- フォーム送信を防ぎながら送信時の処理を指定 -->
+  <form class="addlist" @submit.prevent="addList">
+    <input v-model="title"
+           type="text"
+           class="text-input"
+           placeholder="Add new list"
+    >
+    <button type="submit" class="add-button">
+      Add
+    </button>
+  </form>
+  <!-- ★ここまで追記 -->
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        title: '',
+      }
+    },
+    methods: {
+      addList: function() {
+        this.$store.dispatch('addlist', { title: this.title })
+        this.title = ''
+      },
+    }
+  }
+</script>
