4f18003deb35d72a386ada0105d9dd0d4d216c8e
kamio06 <yakugozy@gmail.com>
2020-10-22 13:02:38 +0900

    add-cardList-commponent

---
+++ src/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- for文でlocalstrageを出力 -->
+  <div class="list">
+    <div class="listheader">
+      <p class="list-title">{{ title }}</p>
+      <div class="deletelist" @click="removeList">
+        X
+      </div>
+      <card-add :listIndex="listIndex" />
+    </div>
+  </div>
+</template>
+
+<script>
+import CardAdd from './CardAdd'
+export default {
+  components: {
+    CardAdd
+  },
+  props: {
+    title: {
+      type:String,
+      required:true
+    },
+    listIndex: {
+      type:Number,
+      required:true
+    }
+  },
+  methods:{
+    removeList:function(){
+      if(confirm("本当にこのリストを削除しますか？")){
+        this.$store.dispatch('removelist',{listIndex: this.listIndex})
+      }
+    },
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
